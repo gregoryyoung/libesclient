d11e91592cfcbb775b5d5609f0f17eb92b4b7fec
Greg Young <gregoryyoung1@gmail.com>
2016-03-09 18:44:40 +0200

    initial zproj output

---
+++ include/libesclient.h
@@ -0,0 +1,16 @@
+/*  =========================================================================
+    libesclient - Project
+
+    Copyright (c) the Authors
+    =========================================================================
+*/
+
+#ifndef LIBESCLIENT_H_H_INCLUDED
+#define LIBESCLIENT_H_H_INCLUDED
+
+//  Include the project library file
+#include "es_library.h"
+
+//  Add your own public definitions here, if you need them
+
+#endif

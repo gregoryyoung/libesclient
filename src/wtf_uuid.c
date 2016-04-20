8b3a71448e5cd2c9083c9be4ded4ef6a9ce090af
Greg Young <gregoryyoung1@gmail.com>
2016-04-21 02:07:51 +0300

    osx returns uppercase for UUID

--- src/wtf_uuid.c
+++ src/wtf_uuid.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <uuid/uuid.h>
 #include "es_client_internal.h"
 
@@ -18,19 +19,28 @@ void write_uuid_to_wtf (uuid_t uuid, unsigned char *wtf_data) {
 
 #ifdef TESTS
 
-void test_read_wtf_uuid() {
+void to_lower (char *str) {
+    for (int i=0; str[i]; i++)
+        str[i] = tolower((unsigned char)str[i]);
+}
+
+void test_read_wtf_uuid () {
     char uuid_str[37];
     char data[16] = {0x46, 0x6c,0xbc, 0x3e, 0x72,0xe2, 0x26, 0x42, 0xbc,0xb5,0xaa,0x93,0xc4,0x11,0xed,0x0d };
     char *expected = "3ebc6c46-e272-4226-bcb5-aa93c411ed0d";
     uuid_t uuid;
     get_uuid_from_wtf (uuid, data);
-    uuid_unparse(uuid, uuid_str);
+    uuid_unparse (uuid, uuid_str);
+    to_lower (uuid_str);
+    printf ("expected %s got %s\n", expected, uuid_str);
     CU_ASSERT_STRING_EQUAL(uuid_str, expected);
     char data1[16] = {0xf8, 0xa6, 0xbb, 0x92, 0xe4, 0x07, 0x4d, 0x4a, 0xb1, 0xdf, 0x19, 0x04, 0xa9, 0x3c, 0x65, 0xa9};
     expected = "92bba6f8-07e4-4a4d-b1df-1904a93c65a9";
     get_uuid_from_wtf (uuid, data1);
-    uuid_unparse(uuid, uuid_str);
-    CU_ASSERT_STRING_EQUAL(uuid_str, expected);
+    uuid_unparse (uuid, uuid_str);
+    to_lower (uuid_str);
+    printf ("expected %s got %s\n", expected, uuid_str);
+    CU_ASSERT_STRING_EQUAL (uuid_str, expected);
 }
 
 void test_write_wtf_uuid() {
